caadbc9a28a1cf33c93d7eeaa00e188e37bad720
saladilakshman <103055304+saladilakshman@users.noreply.github.com>
2024-09-26 18:12:17 +0530

    tested tab color

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Helmet } from "react-helmet";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <Helmet>
+      <meta name="theme-color" content="#9c27b0" />
+      <title>lakshmanjs.vercel.app</title>
+    </Helmet>
     <App />
-  </React.StrictMode>,
-)
+  </>
+);
